d3c89bbb6f5168aac774c841cd28c38e435258ce
Miller Rich <william.miller.rich@gmail.com>
2020-09-20 15:45:38 -0500

    create employee_db database in seed.sql, create tables for employee, role and department, insert test values into each table

--- seed.sql
+++ seed.sql
@@ -11,7 +11,7 @@ CREATE TABLE employee (
     role_id INT NOT NULL,
     manager_id INT,
     PRIMARY KEY (id)
-)
+);
 
 CREATE TABLE role (
     id INT AUTO_INCREMENT NOT NULL,
@@ -19,10 +19,19 @@ CREATE TABLE role (
     salary DECIMAL NOT NULL,
     department_id INT NOT NULL,
     PRIMARY KEY (id)
-)
+);
 
 CREATE TABLE department (
     id INT AUTO_INCREMENT NOT NULL,
     name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
-)
+);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Test", "Person", 1, 1);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Example Title", 60000.66, 1);
+
+INSERT INTO department (name)
+VALUES ("Test Dept");
