e3ab5aaadfc7ce2d0f7c39c26d393b3167e55718
Miller Rich <william.miller.rich@gmail.com>
2020-09-20 17:54:53 -0500

    create mysql connection in index.js, begin questions array in index.js

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 var { prompt } = require("inquirer");
 var mysql = require("mysql");
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'employee_db'
+});
+
+connection.connect();
+
+const questions = [
+    {
+        type: "list",
+        message: "What would you like to do?",
+        name: "options",
+        choices: [
+            "view all employees",
+            "add employee",
+            "remove employee",
+            "view employees by department",
+            "view employees by role"
+        ]
+    }
+]
