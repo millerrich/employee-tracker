03f02392a190f46e1001df60abe259d714ff342c
Miller Rich <william.miller.rich@gmail.com>
2020-09-20 18:00:36 -0500

    add port to mysql connection

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var { prompt } = require("inquirer");
 var mysql = require("mysql");
 var connection = mysql.createConnection({
     host: 'localhost',
+    port: 3306,
     user: 'root',
     password: '',
     database: 'employee_db'
